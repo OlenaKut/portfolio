b39a196f5e5951686ef94b13ecd2f817f507c7a1
OlenaKut <cundrochka@gmail.com>
2023-03-13 21:54:47 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UA from "./pictures/UA.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero">
+        <h5>👋 Hello, I am</h5>
+        <h1>Olena Kutasevych</h1>
+        <h3>
+          Front-end developer, based in Sweden
+          <img src={UA} alt="Ukraine" className="ua m-2" />
+        </h3>
+      </div>
     </div>
   );
 }
