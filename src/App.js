025e25062401c16b5f3caca31c64f35d53e1cae4
OlenaKut <cundrochka@gmail.com>
2023-03-13 22:24:33 +0100

    Added title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import UA from "./pictures/UA.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import Link from "react-bootstrap/Button";
 
 function App() {
   return (
@@ -8,10 +9,20 @@ function App() {
       <div className="hero">
         <h5>👋 Hello, I am</h5>
         <h1>Olena Kutasevych</h1>
-        <h3>
+        <h3 className="mb-4">
           Front-end developer, based in Sweden
           <img src={UA} alt="Ukraine" className="ua m-2" />
         </h3>
+        <button
+          href="/contact.html"
+          className="btn-branding"
+          title="Contact Olena"
+        >
+          Contact me
+        </button>
+        <button href="/about.html" className="btn-branding" title="About Olena">
+          About me
+        </button>
       </div>
     </div>
   );
