6c1e9667ae4bbea82ada2859a1042270a0c0caa9
OlenaKut <cundrochka@gmail.com>
2023-03-15 13:47:53 +0100

    Finished first page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import GitHub from "../pictures/GitHub.png";
+import Facebook_icon from "../pictures/Facebook_icon.png";
+import linkedin from "../pictures/linkedin.png";
+import Instagram_icon from "../pictures/Instagram_icon.webp";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="m-lg-5 m-3">
-      <div className="contact-box d-md-flex d-block text-center text-md-start justify-content-between mb-3 mb-lg-5">
+      <div className="contact-box d-md-flex d-block text-center text-md-start justify-content-between mb-2 mb-lg-4">
         <div>
           <h1 className="footer-h1 mb-3">Work Inquiry</h1>
           <p className="footer-p">
@@ -34,14 +37,26 @@ const Footer = () => {
         >
           cundrochka@gmail.com
         </a>
-        <div className="social-media m-3">
+        <div className="social-media m-4">
           <a
             href="https://github.com/OlenaKut"
             target="_blank"
             title="GitHub Profile"
             rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-github m-2 social-media-link"></i>
+            <img src={GitHub} alt="GitHub" className="m-3 social-media-link" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/olena-kutasevych-b0b99a24b/"
+            target="_blank"
+            title="LinkedIn Profile"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedin}
+              alt="LinkedIn"
+              className="m-3 social-media-link"
+            />
           </a>
           <a
             href="https://www.facebook.com/cundrochka"
@@ -49,18 +64,39 @@ const Footer = () => {
             title="Facebook Profile"
             rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-facebook m-2 social-media-link"></i>
+            <img
+              src={Facebook_icon}
+              alt="Facebook"
+              className="m-3 social-media-link"
+            />
           </a>
+
           <a
             href="https://www.instagram.com/helenkutasevych/"
             target="_blank"
             title="Instagram Profile"
             rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-instagram m-2 social-media-link"></i>
+            <img
+              src={Instagram_icon}
+              alt="Instagram"
+              className="m-3 social-media-link"
+            />
           </a>
         </div>
       </div>
+      <h6 class="text-center">
+        ✌️ This website was coded by Olena Kutasevych, and is
+        <a
+          href="https://github.com/OlenaKut/portfolio"
+          target="_blank"
+          title="GitHub Profile"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced
+        </a>
+      </h6>
     </div>
   );
 };
--- src/pages/home/FirstPage.js
+++ src/pages/home/FirstPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import UA from "../../pictures/UA.png";
@@ -26,6 +27,7 @@ const FirstPage = () => {
         </button>
       </div>
       <Main />
+      <Footer />
     </div>
   );
 };
